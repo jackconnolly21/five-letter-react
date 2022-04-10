d2e25b77e9fb68daaae722196eb518c911f64841
Jack Connolly <jackwconnolly@gmail.com>
2022-04-10 16:55:05 -0400

    fix colors

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -55,29 +55,25 @@ export const Cell = ({
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
-      'bg-yellow-500 dark:bg-yellow-500 text-white border-yellow-500 dark:border-yellow-500':
-        value && overrideStatus === 'maybe',
-      'bg-green-500 dark:bg-green-500 text-white border-green-500 dark:border-green-500':
-        overrideStatus === 'present',
-      'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
-        (!status && !resultStatus) || status === 'none',
-      'border-black dark:border-slate-100': (value && !status) || isResult,
+      'present shadowed bg-yellow-500 dark:bg-yellow-500 text-white border-yellow-500 dark:border-yellow-500':
+        overrideStatus === 'maybe' || resultStatus === 'medium',
+      'present shadowed bg-green-500 dark:bg-green-500 text-white border-green-500 dark:border-green-500':
+        overrideStatus === 'present' ||
+        resultStatus === 'high' ||
+        resultStatus === 'correct',
       'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
         overrideStatus === 'absent',
       'present shadowed bg-red-500 dark:bg-red-500 text-white border-red-500 dark:border-red-500':
         overrideStatus === 'invalid' || resultStatus === 'zero',
-      'present shadowed bg-yellow-500 text-white border-yellow-500':
-        resultStatus === 'medium',
-      'present shadowed bg-green-500 text-white border-green-500':
-        resultStatus === 'high' || resultStatus === 'correct',
+      'border-black dark:border-slate-100': (value && !status) || isResult,
       'cell-fill-animation': isFilled,
       'cell-reveal': shouldReveal,
     }
   )
 
   return (
-    <button onClick={handleClick}>
-      <div className={classes} style={{ animationDelay }} key={key}>
+    <button onClick={handleClick} key={key}>
+      <div className={classes} style={{ animationDelay }}>
         <div className="letter-container" style={{ animationDelay }}>
           {value}
         </div>
