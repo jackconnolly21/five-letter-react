adb27b2d5009707639b3368c0c2dde1bf544df0a
Jack Connolly <jackwconnolly@gmail.com>
2022-05-07 16:44:04 -0400

    fix spacing for clear button

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -1,3 +1,4 @@
+import { CLEAR_NOTES_TEXT, CLEAR_TEXT } from '../../constants/strings'
 import { BaseModal } from './BaseModal'
 import { SettingsToggle } from './SettingsToggle'
 
@@ -26,14 +27,14 @@ export const SettingsModal = ({
         />
         <div className="flex justify-between py-3">
           <div className="text-gray-500 dark:text-gray-300 mt-2 text-left">
-            <p className="leading-none">Clear Notes</p>
+            <p className="leading-none">{CLEAR_NOTES_TEXT}</p>
           </div>
           <button
             type="button"
-            className="rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+            className="rounded-md border border-transparent shadow-sm px-2 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
             onClick={handleClearNotes}
           >
-            Clear
+            {CLEAR_TEXT}
           </button>
         </div>
       </div>
